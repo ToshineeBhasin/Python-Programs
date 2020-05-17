188984ebb6150fbd4c6b592ccce1a0f42b8aee96
ToshineeBhasin <65332593+ToshineeBhasin@users.noreply.github.com>
2020-05-18 00:23:27 +0530

    Add files via upload

---
+++ ASCIIvalueOfCharacter.py
@@ -0,0 +1,31 @@
+'''
+Created on 18-May-2020
+
+@author: Toshinee Bhasin
+'''
+# Program to find the ASCII value of the given character
+
+'''
+ASCII value of number (0-9) is 48-57
+ASCII value of A-Z is 65-90
+ASCII value of a-z is 97-122
+'''
+
+ch=input("Enter character :")
+
+print("The ASCII value of '" + ch + "' is", ord(ch))
+
+
+'''
+Enter character :T
+The ASCII value of 'T' is 84
+or
+Enter character :a
+The ASCII value of 'a' is 97
+or
+Enter character :2
+The ASCII value of '2' is 50
+or
+Enter character :%
+The ASCII value of '%' is 37
+'''
