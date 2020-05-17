cbc89f8d39fd3721fd986dbdf660975132f3cc40
ToshineeBhasin <65332593+ToshineeBhasin@users.noreply.github.com>
2020-05-18 02:42:06 +0530

    Update shape.py

--- shape.py
+++ shape.py
@@ -1,5 +1,5 @@
 import turtle
-#to print square
+#to print square without functiom
 
 toshi=turtle.Screen()
 toshi_turtle=turtle.Turtle()
@@ -12,7 +12,7 @@ toshi_turtle.right(90)
 toshi_turtle.forward(100)
 toshi_turtle.right(90)
 
-#to print star 
+#to print star without function
 
 star = turtle.Turtle() 
 star.forward(50) 
@@ -25,3 +25,25 @@ star.forward(50)
 star.right(144) 
 star.forward(50) 
 star.right(144) 
+
+
+#Square with function
+toshi=turtle.Turtle()
+def square():
+    toshi.forward(100)
+    toshi.right(90)
+    toshi.forward(100)
+    toshi.right(90)
+    toshi.forward(100)
+    toshi.right(90)
+    toshi.forward(100)
+    #toshi.right(90)
+    
+square()
+toshi.forward(50)
+square()
+toshi.forward(50)
+square()
+toshi.forward(50)
+square()
+    
