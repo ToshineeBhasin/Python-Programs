7dc9fa98c12a6f50c45e37919f05c0621f789bff
ToshineeBhasin <65332593+ToshineeBhasin@users.noreply.github.com>
2020-05-18 00:14:24 +0530

    Update HCFof2Nos.py

--- HCFof2Nos.py
+++ HCFof2Nos.py
@@ -1,5 +1,13 @@
+'''
+The highest common factor (H.C.F) or greatest common 
+divisor (G.C.D) of two numbers is the largest positive 
+integer that perfectly divides the two given numbers.
+
+For example, the H.C.F of 12 and 14 is 2.
+'''
+
 #program to find HCF or GCD of two numbers 
-print("Enter two numbers :")
+
 num1=int(input("Enter first number :"))
 num2=int(input("Enter Second number :"))
 if num1>num2:
@@ -9,16 +17,17 @@ else:
     for i in range(1,smaller+1):
         if ((num1%i==0)and(num2%i==0)):
             hcf=i
-print("HCF of given numbers is ",hcf)
+print("HCF of ",num1," and ",num2," is ",hcf)
 
 
 '''
 output:
-Enter two numbers :
-Enter first number :22
-Enter Second number :36
-HCF of given numbers is  2
+Enter first number :12
+Enter Second number :14
+HCF of  12  and  14  is  2
+
 '''
+
 
 
 
