8c7fc03fa6244c4d49f7a4c65c8fd9e3df6f3b9d
ToshineeBhasin <65332593+ToshineeBhasin@users.noreply.github.com>
2020-05-17 23:53:09 +0530

    Add files via upload

---
+++ FactorOfNumber.py
@@ -0,0 +1,45 @@
+'''
+Created on 17-May-2020
+
+@author: Toshinee Bhasin
+'''
+#Program to find factors of a number 
+x=int(input("Enter the number : "))
+print("Factors of", x," are ")
+for i in range(1,x+1):
+    if x%i==0:
+        print(i)
+
+'''
+output:
+Enter the number : 21
+Factors of 21  are 
+1
+3
+7
+21
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ HCFof2Nos.py
@@ -0,0 +1,35 @@
+'''
+Created on 17-May-2020
+
+@author: Toshinee Bhasin
+'''
+#program to find HCF of two numbers 
+print("Enter two numbers :")
+num1=int(input("Enter first number :"))
+num2=int(input("Enter Second number :"))
+if num1>num2:
+    smaller=num2
+else:
+    smaller=num1
+    for i in range(1,smaller+1):
+        if ((num1%i==0)and(num2%i==0)):
+            hcf=i
+print("HCF of given numbers is ",hcf)
+
+
+'''
+output:
+Enter two numbers :
+Enter first number :22
+Enter Second number :36
+HCF of given numbers is  2
+'''
+
+
+
+
+
+
+
+
+
