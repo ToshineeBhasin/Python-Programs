456a93245c4a16630ddbf0b275f7a70e2e69cb71
ToshineeBhasin <65332593+ToshineeBhasin@users.noreply.github.com>
2020-05-18 02:19:11 +0530

    Add files via upload

---
+++ shape.py
@@ -0,0 +1,32 @@
+'''
+Created on 18-May-2020
+
+@author: Toshinee Bhasin
+'''
+import turtle
+#to print square
+
+toshi=turtle.Screen()
+toshi_turtle=turtle.Turtle()
+toshi_turtle.forward(100)
+toshi_turtle.right(90)
+toshi_turtle.forward(100)
+toshi_turtle.right(90)
+toshi_turtle.forward(100)
+toshi_turtle.right(90)
+toshi_turtle.forward(100)
+toshi_turtle.right(90)
+
+#to print star 
+
+star = turtle.Turtle() 
+star.forward(50) 
+star.right(144) 
+star.forward(50) 
+star.right(144) 
+star.forward(50) 
+star.right(144) 
+star.forward(50) 
+star.right(144) 
+star.forward(50) 
+star.right(144) 
