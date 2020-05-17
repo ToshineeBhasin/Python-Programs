2594c800d17615e230fb2a9c2eedd04ab9f333d6
ToshineeBhasin <65332593+ToshineeBhasin@users.noreply.github.com>
2020-05-18 00:15:00 +0530

    Add files via upload

---
+++ LCMof2Nos.py
@@ -0,0 +1,49 @@
+'''
+Created on 17-May-2020
+
+@author: Toshinee Bhasin
+'''
+'''
+The least common multiple (L.C.M.) of two numbers is the 
+smallest positive integer that is perfectly divisible by 
+the two given numbers.
+
+For example, the L.C.M. of 12 and 14 is 84.
+'''
+#program to find LCM of 2 numbers
+num1=int(input("Enter first number :"))
+num2=int(input("Enter second number :"))
+if num1>num2:
+    greater=num1
+else:
+    greater=num2
+    while(True):
+        if ((greater%num1==0) and (greater%num2==0)):
+            lcm=greater
+            break
+        greater=greater+1
+print("LCM of ",num1," and ",num2," is ",lcm)            
+
+'''
+output:
+Enter first number :2
+Enter second number :12
+LCM of  2  and  12  is  12
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
