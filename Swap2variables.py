1f1d8c8b163c9baf8c08fdb9c12809185f1f0dfa
ToshineeBhasin <65332593+ToshineeBhasin@users.noreply.github.com>
2020-05-18 15:10:13 +0530

    Add files via upload

---
+++ Swap2variables.py
@@ -0,0 +1,43 @@
+'''
+Created on 18-May-2020
+
+@author: Toshinee Bhasin
+'''
+#program to swap 2 numbers
+num1=int(input("Enter first number :"))
+num2=int(input("Enter second number :"))
+print("Number before swapping :")
+print("First number :",num1)
+print("Second number :",num2)
+
+temp=num1
+num1=num2
+num2=temp
+
+print("Number after swapping :")
+print("First number :",num1)
+print("Second number :",num2)
+
+'''
+output:
+Enter first number :22
+Enter second number :17
+Number before swapping :
+First number : 22
+Second number : 17
+Number after swapping :
+First number : 17
+Second number : 22
+
+'''
+
+
+
+
+
+
+
+
+
+
+
