959c91b6407428e8ef9d87cdf10d896d9d9094c1
ToshineeBhasin <65332593+ToshineeBhasin@users.noreply.github.com>
2020-05-18 21:09:48 +0530

    Add files via upload

---
+++ calenderProgram.py
@@ -0,0 +1,65 @@
+'''
+Created on 18-May-2020
+
+@author: Toshinee Bhasin
+'''
+import calendar
+'''
+print(calendar.weekheader(3))
+print()
+print(calendar.firstweekday())
+print()
+'''
+print(calendar.month(2020,11))
+print()
+'''
+output:
+   November 2020
+Mo Tu We Th Fr Sa Su
+                   1
+ 2  3  4  5  6  7  8
+ 9 10 11 12 13 14 15
+16 17 18 19 20 21 22
+23 24 25 26 27 28 29
+30
+'''
+
+print(calendar.month(2020,9))
+print()
+
+'''
+output:
+September 2020
+Mo Tu We Th Fr Sa Su
+    1  2  3  4  5  6
+ 7  8  9 10 11 12 13
+14 15 16 17 18 19 20
+21 22 23 24 25 26 27
+28 29 30
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
