8c227328a867b63a5fe47d69dc72e534dfaed03b
ToshineeBhasin <65332593+ToshineeBhasin@users.noreply.github.com>
2020-05-20 18:21:22 +0530

    Update dateTimeModule.py

--- dateTimeModule.py
+++ dateTimeModule.py
@@ -1,8 +1,3 @@
-'''
-Created on 20-May-2020
-
-@author: Toshinee Bhasin
-'''
 import datetime
 x = datetime.datetime.now()
 print(x)
@@ -11,13 +6,10 @@ print(x)
 output:
 2020-05-20 17:09:26.610724
 '''
-
-import datetime
-
 x = datetime.datetime.now()
-
 print(x.year)
 print(x.strftime("%A"))
+
 '''
 output:
 2020
