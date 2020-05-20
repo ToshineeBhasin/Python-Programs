d0c0037e7fc0444517a08fc714db57d790e7fb08
ToshineeBhasin <65332593+ToshineeBhasin@users.noreply.github.com>
2020-05-20 18:22:27 +0530

    Update pythonIterator.py

--- pythonIterator.py
+++ pythonIterator.py
@@ -1,9 +1,4 @@
 '''
-Created on 20-May-2020
-
-@author: Toshinee Bhasin
-'''
-'''
 An iterator is an object that contains a countable number of values.
 An iterator is an object that can be iterated upon, meaning that you 
 can traverse through all the values. Technically, in Python, an iterator 
