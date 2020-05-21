43f2c314195f4e9e2f6b92e117028ee4c2d782eb
ToshineeBhasin <65332593+ToshineeBhasin@users.noreply.github.com>
2020-05-21 23:15:27 +0530

    Add files via upload

---
+++ BotGame.py
@@ -0,0 +1,26 @@
+'''
+Created on 21-May-2020
+
+@author: Toshinee Bhasin
+'''
+from PIL import ImageGrab
+import pyautogui
+from time import sleep
+class Cordinates():
+    replayBtn = (799,386)
+    dinasaur = (562,400)
+    
+def restartGame():
+    pyautogui.click(Cordinates.replayBtn)
+    
+restartGame()
+
+def pressSpace():
+    pyautogui.keyDown('space')
+    sleep(0.05)
+    print("Jump")
+    pyautogui.keyUp('space')
+    
+restartGame()
+sleep(1)
+pressSpace()
