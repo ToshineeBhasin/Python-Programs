3ab764b351722e7eda9a253eb708fea18e71ac57
ToshineeBhasin <65332593+ToshineeBhasin@users.noreply.github.com>
2020-05-28 17:58:56 +0530

    Add files via upload

---
+++ May26Set.py
@@ -0,0 +1,36 @@
+'''
+Created on 28-May-2020
+
+@author: Toshinee Bhasin
+'''
+s={1,2,4,6,7}
+print(s)        #{1, 2, 4, 6, 7}
+s.add(9)
+print(s)        #{1, 2, 4, 6, 7, 9}
+s.add((2,3))
+print(s)        #{1, 2, 4, 6, 7, (2, 3), 9}
+s.clear()
+print(s)        #set()
+
+#concatenate list
+l1=[2,3,4,5]
+l2=[3,5,7,9]
+l3=list(l1)
+print(l3)       #[2, 3, 4, 5]
+l3.extend(l2)
+print(l3)       #[2, 3, 4, 5, 3, 5, 7, 9]
+l3=list(set(l3))
+print(l3)       #[2, 3, 4, 5, 7, 9]
+
+print(set(l1)|set(l2))      #{2, 3, 4, 5, 7, 9}
+
+s1=set(l1)
+s2=set(l2)
+print(s1.union(s2))     #{2, 3, 4, 5, 7, 9}
+print(s1|s2)            #{2, 3, 4, 5, 7, 9}
+
+print(s1.intersection(s2))      #{3, 5}
+print(s1&s2)                    #{3, 5}
+
+print(s1.difference(s2))        #{2, 4}
+print(s1-s2)                    #{2, 4}
