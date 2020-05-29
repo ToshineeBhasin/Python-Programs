c8b2e7320b290f45bd06f37c0c6ab2bd12bb4f24
ToshineeBhasin <65332593+ToshineeBhasin@users.noreply.github.com>
2020-05-29 23:41:22 +0530

    Update Set-operations.py

--- Set-operations.py
+++ Set-operations.py
@@ -30,3 +30,10 @@ print(s1&s2)                    #{3, 5}
 
 print(s1.difference(s2))        #{2, 4}
 print(s1-s2)                    #{2, 4}
+
+
+print(s1.difference_update(s2))     #None
+print(s1,s2)            #{2, 4} {9, 3, 5, 7}
+
+print(s1,s2)            #{2, 4} {9, 3, 5, 7}
+print(s1.symmetric_difference(s2))      #{2, 3, 4, 5, 7, 9}
