2e5d7b2abbf5272b6cbcafd728d6f285d08cd81b
ToshineeBhasin <65332593+ToshineeBhasin@users.noreply.github.com>
2020-05-29 23:24:41 +0530

    Add files via upload

---
+++ May29Programs.py
@@ -0,0 +1,27 @@
+'''
+Created on 29-May-2020
+
+@author: Toshinee Bhasin
+'''
+#program to check whether a number is valid or invalid
+valid=0
+invalid=0
+for i in range(5):
+    try:
+        n=int(input("Enter any number :"))
+        valid+=1
+    except Exception:
+        invalid+=1
+        
+print("Entered valid numbers :",valid)
+print("Entered invalid numbers :",invalid)
+'''
+output:
+Enter any number :0
+Enter any number :-7
+Enter any number :5
+Enter any number :9
+Enter any number :j
+Entered valid numbers : 4
+Entered invalid numbers : 1
+'''
