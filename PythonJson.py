ddc5ab369b4ca7da2bf9229164f61da3f3b9a7da
ToshineeBhasin <65332593+ToshineeBhasin@users.noreply.github.com>
2020-05-31 19:17:22 +0530

    Add files via upload

---
+++ PythonJson.py
@@ -0,0 +1,90 @@
+'''
+Created on 31-May-2020
+
+@author: Toshinee Bhasin
+'''
+'''
+JSON stands for JavaScript Object Notation
+JSON is a lightweight format for storing and transporting data
+JSON is often used when data is sent from a server to a web page
+JSON is "self-describing" and easy to understand
+'''
+#Convert from JSON to Python:
+import json
+
+# some JSON:
+x = '{ "name":"Toshi", "age":30, "city":"Pune"}'
+
+# parse x:
+y = json.loads(x)
+
+# the result is a Python dictionary:
+print(y["age"])         #30
+print(y["name"])        #Toshi
+print(y["city"])        #Pune
+print(y)                #{'name': 'Toshi', 'age': 30, 'city': 'Pune'}
+
+
+#Convert from Python to JSON:
+# a Python object (dict):
+x = {
+  "name": "Toshi",
+  "age": 30,
+  "city": "Pune"
+}
+
+# convert into JSON:
+y = json.dumps(x)
+
+# the result is a JSON string:
+print(y)            #{"name": "Toshi", "age": 30, "city": "Pune"}
+
+
+#Convert Python objects into JSON strings, and print the values:
+print(json.dumps({"name": "John", "age": 30}))
+print(json.dumps(["apple", "bananas"]))
+print(json.dumps(("apple", "bananas")))
+print(json.dumps("hello"))
+print(json.dumps(42))
+print(json.dumps(31.76))
+print(json.dumps(True))
+print(json.dumps(False))
+print(json.dumps(None))
+
+'''
+output:
+{"name": "John", "age": 30}
+["apple", "bananas"]
+["apple", "bananas"]
+"hello"
+42
+31.76
+true
+false
+null
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
