c810e9b0c631216c928bf685f48280c5fcc172a2
ToshineeBhasin <65332593+ToshineeBhasin@users.noreply.github.com>
2020-06-01 16:25:47 +0530

    Update PythonFile.py

--- PythonFile.py
+++ PythonFile.py
@@ -61,3 +61,50 @@ f.close()
 
 #output : Hello Everyone!!!!!!!
 
+'''
+Write to an Existing File
+To write to an existing file, you must add a parameter to the open() function:
+1)  "a" - Append - will append to the end of the file
+2)  "w" - Write - will overwrite any existing content
+'''
+
+#Open the file "demofile2.txt" and append content to the file:
+f = open("toshi.txt", "a")
+f.write("Now the file has more content!")
+f.close()
+
+#open and read the file after the appending:
+f = open("toshi.txt", "r")
+print(f.read())
+
+'''
+output:
+Hello Everyone!!!!!!!
+
+This is text file for python.Now the file has more content!
+'''
+
+'''
+Create a New File
+To create a new file in Python, use the open() method, with one of the following parameters:
+1)  "x" - Create - will create a file, returns an error if the file exist
+2)  "a" - Append - will create a file if the specified file does not exist
+3)  "w" - Write - will create a file if the specified file does not exist
+'''
+f = open("myfile.txt", "x")     #Create a file called "myfile.txt"
+f = open("myfile.txt", "w")     #Create a new file if it does not exist:
+
+
+#Remove the file "demofile.txt":
+
+import os
+os.remove("toshi.txt")
+
+
+#Check if file exists, then delete it:
+
+if os.path.exists("demofile.txt"):
+    os.remove("demofile.txt")
+else:
+    print("The file does not exist")
+
