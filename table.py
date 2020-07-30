a4347e27308fb00cf2d8536d416095287fff6844
ToshineeBhasin <65332593+ToshineeBhasin@users.noreply.github.com>
2020-07-30 11:12:51 +0530

    Update table.py

--- table.py
+++ table.py
@@ -1,8 +1,4 @@
-'''
-Created on 25-Jul-2020
-
-@author: Toshinee Bhasin
-'''
+#python program to print a table
 num = eval(input("Enter a number :"))
 for i in range(1,11):
     print(num, " * ",i," = ",num*i)
