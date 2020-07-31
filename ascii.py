9cace759f36a5af1f563fe964026b5fa2c7242bb
ToshineeBhasin <65332593+ToshineeBhasin@users.noreply.github.com>
2020-07-31 11:43:06 +0530

    to find ascii value

---
+++ ascii.py
@@ -0,0 +1,14 @@
+'''
+Created on 30-Jul-2020
+
+@author: Toshinee Bhasin
+'''
+# Program to find the ASCII value of the given character
+
+c = input("Enter the character :")
+print("The ASCII value of '" + c + "' is", ord(c))
+'''
+output:
+Enter the character :t
+The ASCII value of 't' is 116
+'''
